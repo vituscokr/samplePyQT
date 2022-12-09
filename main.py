88bc1be168c2ae3cbff881ee6b5701912cf45be0
namgt365 <namgt@doubled.kr>
2022-12-09 16:23:33 +0900

    2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QWidget , QPushButton, QMainWindow
+from PySide6.QtWidgets import QApplication, QWidget , QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout
 from PySide6.QtCore import QSize, Qt
 from random import choice
 
@@ -25,13 +25,30 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
+        # 라벨 
+        self.label = QLabel()
+        # input
+        self.input = QLineEdit() 
+        self.input.textChanged.connect(self.label.setText)
+        # Button 
         self.button = QPushButton("Press Me!")
-#        self.button.setCheckable(True)
         self.button.clicked.connect(self.clicked_button)
         self.windowTitleChanged.connect(self.changed_window_title)
-#        self.button.clicked.connect(self.toggled_button)
-        self.setFixedSize(QSize(400, 300))
-        self.setCentralWidget(self.button)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label) 
+        layout.addWidget(self.button) 
+
+        container = QWidget()
+        container.setLayout(layout) 
+        self.setCentralWidget(container) 
+#         
+# #        self.button.setCheckable(True)
+
+# #        self.button.clicked.connect(self.toggled_button)
+#         self.setFixedSize(QSize(400, 300))
+#         self.setCentralWidget(self.button)
 
 
     def clicked_button(self):
